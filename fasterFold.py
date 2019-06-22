b1853ba0a4707e2ca8ab950f0888d180b4f69872
merrycatch22 <tarn.nicholas@gmail.com>
2019-06-22 15:37:34 -0700

    just how long will these creds last

--- fasterFold.py
+++ fasterFold.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from time import sleep
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchWindowException
 from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+import pickle
 
 if __name__ == '__main__':
 
@@ -15,6 +17,19 @@ if __name__ == '__main__':
     chrome_options.add_argument('--always-authorized-plugins=true')
     chrome_options.add_argument("--disable-infobars")
     chrome_options.add_extension('uBlock-Origin_v1.20.0.crx')
+    chrome_options.add_argument("--user-data-dir=chrome-data")
     browser = webdriver.Chrome('chromedriver.exe',chrome_options=chrome_options)
+    
+    # cookies = pickle.load(open("cookies.pkl", "rb"))
+    # for cookie in cookies:
+    #     print (cookie)
+    #     try:
+    #         cookie.pop('expiry')
+    #     except KeyError:
+    #         pass
+    #     browser.add_cookie(cookie)
 
     browser.get("https://www.bovada.lv/poker-lobby/home")
+    # sleep(60)
+    # print('times up')
+    # pickle.dump( browser.get_cookies() , open("cookies.pkl","wb"))
